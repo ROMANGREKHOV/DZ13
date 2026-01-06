53bc487f4a9565913db58db449f4ee96f1e8d379
ROMANGREKHOV <94071555+ROMANGREKHOV@users.noreply.github.com>
2026-01-06 11:11:56 +0300

    DZ13_3.cpp

--- DZ13_3.cpp
+++ DZ13_3.cpp
@@ -1,27 +1,28 @@
-//#include <iostream>
-//#include <fstream>
-//
-//int main()
-//{
-//	int size{};
-//	int* arr = new int[size]();
-//	std::cout << "Введите размер массива: ";
-//	std::cin >> size;
-//	for (int i = 0; i < size; ++i)
-//	{
-//		std::cout << "arr[" << i << "] = ";
-//		std::cin >> arr[i];
-//	}
-//	std::ofstream ofile("out.txt");
-//	if (ofile.is_open())
-//	{
-//		ofile << size << std::endl;
-//		for (int i = size - 1; i >= 0; --i)
-//		{
-//			ofile << arr[i] << ' ';
-//		}
-//	}
-//	ofile.close();
-//
-//	return EXIT_SUCCESS;
-//}
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+	int size{};
+	int* arr = new int[size]();
+	std::cout << "Введите размер массива: ";
+	std::cin >> size;
+	for (int i = 0; i < size; ++i)
+	{
+		std::cout << "arr[" << i << "] = ";
+		std::cin >> arr[i];
+	}
+	std::ofstream ofile("out.txt");
+	if (ofile.is_open())
+	{
+		ofile << size << std::endl;
+		for (int i = size - 1; i >= 0; --i)
+		{
+			ofile << arr[i] << ' ';
+		}
+	}
+	ofile.close();
+
+	return EXIT_SUCCESS;
+
+}
